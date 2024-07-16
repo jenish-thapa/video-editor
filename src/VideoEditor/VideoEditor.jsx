7c786c8cd30f8a6dfc86c7a729cef4d8afe5407b
Beserker-356 <jenish.23bcs10016@ms.sst.scaler.com>
2024-07-16 12:09:44 +0530

    Added Dynamic Preview

--- src/VideoEditor/VideoEditor.jsx
+++ src/VideoEditor/VideoEditor.jsx
@@ -1,8 +1,11 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import ReactPlayer from "react-player";
 import Draggable from "react-draggable";
 import { FaPlay, FaPause } from "react-icons/fa";
 import { HiSpeakerWave } from "react-icons/hi2";
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { fetchFile, toBlobURL } from "@ffmpeg/util";
+import { Observable } from "rxjs";
 import "./VideoEditor.css";
 
 const VideoEditor = () => {
@@ -14,17 +17,57 @@ const VideoEditor = () => {
   const [duration, setDuration] = useState(0);
   const [speed, setSpeed] = useState(1.0);
   const [aspectRatio, setAspectRatio] = useState("16:9");
-  const [width, setWidth] = useState(0);
   const videoRef = useRef();
 
-  const [videoHeight, setVideoHeight] = useState(0);
+  const [cropperWidth, setCropperWidth] = useState(0);
+  const [leftWidth, setLeftWidth] = useState(0);
+  const [rightWidth, setRightWidth] = useState(0);
+  const [prePos, setPrePos] = useState(0);
 
-  // Callback for when the video player is ready
-  const handlePlayerReady = (player) => {
-    // Accessing the player's client dimensions
-    const height = player.wrapper.clientHeight;
-    setVideoHeight(height);
+  const [loaded, setLoaded] = useState(false);
+  const ffmpegRef = useRef(new FFmpeg());
+  const preVideoRef = useRef(null);
+
+  const [cropPos, setCropPos] = useState({
+    x: 0,
+    y: 0,
+  });
+  const videoDimensions = {
+    height: 450,
+    width: 800,
   };
+
+  useEffect(() => {
+    const load = async () => {
+      const baseURL = "https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd";
+      const ffmpeg = ffmpegRef.current;
+      ffmpeg.on("log", ({ message }) => {
+        console.log(message);
+      });
+      // toBlobURL is used to bypass CORS issue, urls with the same
+      // domain can be used directly.
+      await ffmpeg.load({
+        coreURL: await toBlobURL(
+          `${baseURL}/ffmpeg-core.js`,
+          "text/javascript"
+        ),
+        wasmURL: await toBlobURL(
+          `${baseURL}/ffmpeg-core.wasm`,
+          "application/wasm"
+        ),
+      });
+      setLoaded(true);
+    };
+
+    load();
+  }, []);
+
+  // const handlePlayerReady = (player) => {
+  //   const height = player.wrapper.clientHeight;
+  //   setVideoHeight(height);
+  //   const newWidth = (height * 16) / 9;
+  //   setCropperWidth(newWidth - 6);
+  // };
 
   const handleVideoUpload = (e) => {
     const file = e.target.files[0];
@@ -84,11 +127,46 @@ const VideoEditor = () => {
     const ratio = e.target.value;
     setAspectRatio(ratio);
     const [w, h] = ratio.split(":").map(Number);
-    const height = videoHeight;
-    console.log(height);
+    const height = videoDimensions.height;
+    // console.log(height);
     const newWidth = (height * w) / h;
-    console.log(newWidth-6);
-    setWidth(newWidth-6);
+    // console.log(newWidth - 6);
+    setCropperWidth(newWidth - 6);
+  };
+  // const crop = async () => {
+  //   const ffmpeg = ffmpegRef.current;
+  //   const { x, y } = cropParams;
+  //   console.log(cropParams);
+  //   await ffmpeg.writeFile("input.mp4", await fetchFile(video));
+  //   const w = width;
+  //   const factorW = 320 / 842.39;
+  //   console.log(factorW * w);
+  //   console.log(factorW * x);
+  //   await ffmpeg.exec([
+  //     "-i",
+  //     "input.mp4",
+  //     "-vf",
+  //     `crop=${w * factorW}:${180}:${x * factorW}:${y}`,
+  //     "output.mp4",
+  //   ]);
+  //   const data = await ffmpeg.readFile("output.mp4");
+  //   preVideoRef.current.src = URL.createObjectURL(
+  //     new Blob([data.buffer], { type: "video/mp4" })
+  //   );
+  // };
+
+  const preview = () => {};
+
+  const handleDrag = (e, ui) => {
+    const { x, y } = cropPos;
+    setCropPos({
+      x: x + ui.deltaX,
+      y: y + ui.deltaY,
+    });
+    setLeftWidth(0.625 * x);
+    setRightWidth(500 - (leftWidth + 0.625 * cropperWidth));
+    setPrePos(250 - (500 - (leftWidth + rightWidth)) / 2 - leftWidth);
+    console.log(x, y);
   };
 
   return (
@@ -108,12 +186,12 @@ const VideoEditor = () => {
                 playbackRate={speed}
                 onProgress={handleProgress}
                 onDuration={handleDuration}
-                onReady={handlePlayerReady}
               />
               <Draggable
                 // nodeRef={videoRef}
                 bounds="parent"
                 defaultPosition={{ x: 0, y: 0 }}
+                onDrag={handleDrag}
               >
                 <div
                   style={{
@@ -123,7 +201,7 @@ const VideoEditor = () => {
                     display: "grid",
                     gridTemplateColumns: "repeat(3, 1fr)",
                     gridTemplateRows: "repeat(3, 1fr)",
-                    width: width,
+                    width: cropperWidth,
                   }}
                 >
                   {[...Array(9)].map((_, i) => (
@@ -217,10 +295,58 @@ const VideoEditor = () => {
             </select>
           </div>
         </div>
+        {loaded ? (
+          <button onClick={preview}>Preview</button>
+        ) : (
+          <button onClick={preview} disabled>
+            Preview
+          </button>
+        )}
       </section>
       <section className="main-right">
         <div id="pre-head">Preview</div>
         <div id="pre-body">
+          <div
+            style={{
+              position: "relative",
+              width: "500px",
+              aspectRatio: "16/9",
+              // transform: `translateX(${prePos}px)`,
+            }}
+          >
+            <div
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                height: "100%",
+                width: leftWidth,
+                backgroundColor: "#37393f",
+              }}
+            ></div>
+            <ReactPlayer
+              ref={preVideoRef}
+              url={video}
+              playing={playing}
+              controls={false}
+              volume={volume}
+              width="100%"
+              height="100%"
+              playbackRate={speed}
+              onProgress={handleProgress}
+              onDuration={handleDuration}
+            />
+            <div
+              style={{
+                position: "absolute",
+                top: 0,
+                right: 0,
+                height: "100%",
+                width: rightWidth,
+                backgroundColor: "#37393f",
+              }}
+            ></div>
+          </div>
           <p id="pre-message">Preview not available</p>
           <p>
             Please click on "Start Cropper"
